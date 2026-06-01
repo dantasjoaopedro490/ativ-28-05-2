d1075e4c4f2f339f8c62f7989433005c07479531
joão pedro <dantasjoaopedro490@gmail.com>
2026-06-01 19:45:15 -0300

    commit mod

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 const carros = []
-const carro = {}
 let contador = 0
+let resposta
 
-alert("Você preencherá uma lista de carros. Esta pronto?")
+alert("Você preencherá uma lista de carros. Está pronto?")
 
 do {
     const carro = {}
 
     carro.marca = prompt("Qual é a marca desse carro?")
-    carro.modelo = prompt("Qual é o modelo desse carro")
-    carro.cor = prompt("Qual é a cor desse carro")
-    carro.potencia = prompt("Qual a potência desse carro")
-    carro.preco = prompt("Qual é o preço desse carro")
+    carro.modelo = prompt("Qual é o modelo desse carro?")
+    carro.cor = prompt("Qual é a cor desse carro?")
+    carro.potencia = prompt("Qual a potência desse carro?")
+    carro.preco = prompt("Qual é o preço desse carro?")
 
     carros.push(carro)
 
     resposta = confirm("Deseja continuar inserindo carros?")
-} while (resposta == true)
+} while (resposta)
 
 do {
     alert(`SEU CARRO
-        Marca: ${carros[contador].marca}
-        Modelo: ${carros[contador].modelo}
-        Cor: ${carros[contador].cor}
-        Potência: ${carros[contador].potencia}
-        Preço: ${carros[contador].preco}
-        `)
-        contador++
-} while (contador<carros.lengh);
+Marca: ${carros[contador].marca}
+Modelo: ${carros[contador].modelo}
+Cor: ${carros[contador].cor}
+Potência: ${carros[contador].potencia}
+Preço: ${carros[contador].preco}`)
+
+    contador++
+} while (contador < carros.length)
+
+alert(`Características dos carros que você escolheu: ${carros.length}`)
